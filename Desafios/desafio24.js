d58588c4bcb740759fef8dd8bb79d946606d81c8
jpvonsohsten <jpvonsohsten@hotmail.com>
2022-10-10 14:51:28 -0300

    commit3

---
+++ Desafios/desafio24.js
@@ -0,0 +1,24 @@
+// Crie um programa que imprima 11 vezes a frase " Hello World!" utilizando uma estrutura de repetição while.
+
+
+function HelloWorld() {
+    let i = 0
+    while (i < 11) {
+    console.log("Hello World")
+    i++
+    }
+}
+
+HelloWorld()
+
+
+//-------------------------------------------------------------------------//---------------------------------------------------------------------
+
+
+function olaMundo() {
+    let repeticao = 0 
+    for(repeticao < 11; repeticao <= 11; repeticao++)
+    console.log("Hello World")
+}
+
+olaMundo()
